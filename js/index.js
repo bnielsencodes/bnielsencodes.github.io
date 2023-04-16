9603e03daf4e6dae93b09ab0ef3dae7d04b07b18
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-04-16 01:54:18 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,31 +6,24 @@ VARIABLES
 const htmlLeftBorder = document.querySelector(".hex-border__left--html");
 const htmlMiddleBorder = document.querySelector(".hex-border__middle--html");
 const htmlRightBorder = document.querySelector(".hex-border__right--html");
-const htmlIcon = document.querySelector(".fa-html5");
+const htmlSVG = document.querySelector(".svg--html");
+const htmlColor1 = document.querySelector(".svg--html-color1");
+const htmlColor2 = document.querySelector(".svg--html-color2");
+const htmlColor3 = document.querySelector(".svg--html-color3");
+const htmlColor4 = document.querySelector(".svg--html-color4");
 const htmlIconP = document.querySelector(".skill__p--html");
 // VITE
 const viteLeftBorder = document.querySelector(".hex-border__left--vite");
 const viteMiddleBorder = document.querySelector(".hex-border__middle--vite");
 const viteRightBorder = document.querySelector(".hex-border__right--vite");
-const viteSVG = document.querySelector(".vite-svg");
-const viteStroke = document.querySelector(".vite-stroke");
-const viteColor1 = document.querySelector(".vite-color1");
-const viteColor2 = document.querySelector(".vite-color2");
-const viteColor3 = document.querySelector(".vite-color3");
-const viteColor4 = document.querySelector(".vite-color4");
-const viteColor5 = document.querySelector(".vite-color5");
+const viteSVG = document.querySelector(".svg--vite");
+const viteStroke = document.querySelector(".svg--vite-stroke");
+const viteColor1 = document.querySelector(".svg--vite-color1");
+const viteColor2 = document.querySelector(".svg--vite-color2");
+const viteColor3 = document.querySelector(".svg--vite-color3");
+const viteColor4 = document.querySelector(".svg--vite-color4");
+const viteColor5 = document.querySelector(".svg--vite-color5");
 const viteIconP = document.querySelector(".skill__p--vite");
-// VS CODE
-// const vscodeLeftBorder = document.querySelector(".hex-border__left--vscode");
-// const vscodeMiddleBorder = document.querySelector(
-//   ".hex-border__middle--vscode"
-// );
-// const vscodeRightBorder = document.querySelector(".hex-border__right--vscode");
-// const vscodeSVG = document.querySelector(".vscode-svg");
-// const vscodePathOne = document.querySelector(".vscode-path-one");
-// const vscodePathTwo = document.querySelector(".vscode-path-two");
-// const vscodePathThree = document.querySelector(".vscode-path-three");
-// const vscodeIconP = document.querySelector(".skill__p--vscode");
 // GIT
 const gitLeftBorder = document.querySelector(".hex-border__left--git");
 const gitMiddleBorder = document.querySelector(".hex-border__middle--git");
@@ -41,13 +34,19 @@ const gitIconP = document.querySelector(".skill__p--git");
 const cssLeftBorder = document.querySelector(".hex-border__left--css");
 const cssMiddleBorder = document.querySelector(".hex-border__middle--css");
 const cssRightBorder = document.querySelector(".hex-border__right--css");
-const cssIcon = document.querySelector(".fa-css3-alt");
+const cssSVG = document.querySelector(".svg--css");
+const cssColor1 = document.querySelector(".svg--css-color1");
+const cssColor2 = document.querySelector(".svg--css-color2");
+const cssColor3 = document.querySelector(".svg--css-color3");
+const cssColor4 = document.querySelector(".svg--css-color4");
+const cssColor5 = document.querySelector(".svg--css-color5");
+const cssColor6 = document.querySelector(".svg--css-color6");
 const cssIconP = document.querySelector(".skill__p--css");
 // SASS
 const sassLeftBorder = document.querySelector(".hex-border__left--sass");
 const sassMiddleBorder = document.querySelector(".hex-border__middle--sass");
 const sassRightBorder = document.querySelector(".hex-border__right--sass");
-const sassIcon = document.querySelector(".fa-sass");
+const sassIcon = document.querySelector(".devicon-sass-original");
 const sassIconP = document.querySelector(".skill__p--sass");
 // NEXTJS
 const nextjsLeftBorder = document.querySelector(".hex-border__left--nextjs");
@@ -68,19 +67,21 @@ const tailwindRightBorder = document.querySelector(
   ".hex-border__right--tailwind"
 );
 const tailwindIconP = document.querySelector(".skill__p--tailwind");
-const tailwindSVG = document.querySelector(".tailwind-svg");
-const tailwindSVGPath = document.querySelector(".tailwind-svg-path");
+const tailwindSVG = document.querySelector(".svg--tailwind");
+const tailwindSVGPath = document.querySelector(".svg__path--tailwind");
 // JAVASCRIPT
 const jsLeftBorder = document.querySelector(".hex-border__left--js");
 const jsMiddleBorder = document.querySelector(".hex-border__middle--js");
 const jsRightBorder = document.querySelector(".hex-border__right--js");
-const jsIcon = document.querySelector(".skill-fa--js");
 const jsIconP = document.querySelector(".skill__p--js");
+const jsSVG = document.querySelector(".svg--js");
+const jsColor1 = document.querySelector(".svg--js-color1");
+const jsColor2 = document.querySelector(".svg--js-color2");
 // REACT
 const reactLeftBorder = document.querySelector(".hex-border__left--react");
 const reactMiddleBorder = document.querySelector(".hex-border__middle--react");
 const reactRightBorder = document.querySelector(".hex-border__right--react");
-const reactIcon = document.querySelector(".fa-react");
+const reactIcon = document.querySelector(".devicon-react-original");
 const reactIconP = document.querySelector(".skill__p--react");
 // FIGMA
 const figmaLeftBorder = document.querySelector(".hex-border__left--figma");
@@ -88,10 +89,21 @@ const figmaMiddleBorder = document.querySelector(".hex-border__middle--figma");
 const figmaRightBorder = document.querySelector(".hex-border__right--figma");
 const figmaIcon = document.querySelector(".fa-figma");
 const figmaIconP = document.querySelector(".skill__p--figma");
+// VS CODE
+// const vscodeLeftBorder = document.querySelector(".hex-border__left--vscode");
+// const vscodeMiddleBorder = document.querySelector(
+//   ".hex-border__middle--vscode"
+// );
+// const vscodeRightBorder = document.querySelector(".hex-border__right--vscode");
+// const vscodeSVG = document.querySelector(".svg--vscode");
+// const vscodePathOne = document.querySelector(".svg--vscode-path1");
+// const vscodePathTwo = document.querySelector(".svg--vscode-path2");
+// const vscodePathThree = document.querySelector(".svg--vscode-path3");
+// const vscodeIconP = document.querySelector(".skill__p--vscode");
 
 /* ------------------------------------
----------------------------------------
-tsPARTICALS FOR HERO BACKGROUND ANIMATION
+    ---------------------------------------
+    tsPARTICLES FOR HERO BACKGROUND ANIMATION
 ---------------------------------------
 ------------------------------------ */
 
@@ -146,7 +158,7 @@ tsParticles.load("tsparticles", {
       enable: true,
       out_mode: "out",
       random: true,
-      speed: 0.2,
+      speed: 1,
       straight: false,
     },
     number: {
@@ -259,32 +271,48 @@ SKILLS ICON MOUSEOVER ANIMATIONS
 ------------------------------------ */
 
 // HTML ICON HOVER ANIMATION
+htmlSVG.addEventListener("mouseover", () => {
+  htmlLeftBorder.classList.add("hex-border__left-hover--html");
+  htmlMiddleBorder.classList.add("hex-bg-color--html");
+  htmlRightBorder.classList.add("hex-border__right-hover--html");
+  htmlIconP.classList.add("hover-color--html");
+  htmlColor1.setAttribute("style", "fill: #E44D26");
+  htmlColor2.setAttribute("style", "fill: #f16529");
+  htmlColor3.setAttribute("style", "fill: #EBEBEB");
+  htmlColor4.setAttribute("style", "fill: #fff");
+});
+
+htmlSVG.addEventListener("mouseout", () => {
+  htmlLeftBorder.classList.remove("hex-border__left-hover--html");
+  htmlMiddleBorder.classList.remove("hex-bg-color--html");
+  htmlRightBorder.classList.remove("hex-border__right-hover--html");
+  htmlIconP.classList.remove("hover-color--html");
+  htmlColor1.setAttribute("style", "fill: #fafafa");
+  htmlColor2.setAttribute("style", "fill: #fafafa");
+  htmlColor3.setAttribute("style", "fill: #272727");
+  htmlColor4.setAttribute("style", "fill: #272727");
+});
+
 htmlIconP.addEventListener("mouseover", () => {
   htmlLeftBorder.classList.add("hex-border__left-hover--html");
   htmlMiddleBorder.classList.add("hex-bg-color--html");
   htmlRightBorder.classList.add("hex-border__right-hover--html");
-  htmlIcon.classList.add("hover-color--html");
+  htmlIconP.classList.add("hover-color--html");
+  htmlColor1.setAttribute("style", "fill: #E44D26");
+  htmlColor2.setAttribute("style", "fill: #f16529");
+  htmlColor3.setAttribute("style", "fill: #EBEBEB");
+  htmlColor4.setAttribute("style", "fill: #fff");
 });
 
 htmlIconP.addEventListener("mouseout", () => {
   htmlLeftBorder.classList.remove("hex-border__left-hover--html");
   htmlMiddleBorder.classList.remove("hex-bg-color--html");
   htmlRightBorder.classList.remove("hex-border__right-hover--html");
-  htmlIcon.classList.remove("hover-color--html");
-});
-
-htmlIcon.addEventListener("mouseover", () => {
-  htmlLeftBorder.classList.add("hex-border__left-hover--html");
-  htmlMiddleBorder.classList.add("hex-bg-color--html");
-  htmlRightBorder.classList.add("hex-border__right-hover--html");
-  htmlIconP.classList.add("hover-color--html");
-});
-
-htmlIcon.addEventListener("mouseout", () => {
-  htmlLeftBorder.classList.remove("hex-border__left-hover--html");
-  htmlMiddleBorder.classList.remove("hex-bg-color--html");
-  htmlRightBorder.classList.remove("hex-border__right-hover--html");
   htmlIconP.classList.remove("hover-color--html");
+  htmlColor1.setAttribute("style", "fill: #fafafa");
+  htmlColor2.setAttribute("style", "fill: #fafafa");
+  htmlColor3.setAttribute("style", "fill: #272727");
+  htmlColor4.setAttribute("style", "fill: #272727");
 });
 
 // VITE SVG HOVER ANIMATION
@@ -340,47 +368,6 @@ viteIconP.addEventListener("mouseout", () => {
   viteColor5.setAttribute("style", "stop-color: #fafafa");
 });
 
-// VS CODE SVG HOVER ANIMATION
-// vscodeSVG.addEventListener("mouseover", () => {
-//   vscodeLeftBorder.classList.add("hex-border__left-hover--vscode");
-//   vscodeMiddleBorder.classList.add("hex-bg-color--vscode");
-//   vscodeRightBorder.classList.add("hex-border__right-hover--vscode");
-//   vscodeIconP.classList.add("hover-color--vscode");
-//   vscodePathOne.setAttribute("style", "fill: #0065A9");
-//   vscodePathTwo.setAttribute("style", "fill: #007ACC");
-//   vscodePathThree.setAttribute("style", "fill: #1F9CF0");
-// });
-
-// vscodeSVG.addEventListener("mouseout", () => {
-//   vscodeLeftBorder.classList.remove("hex-border__left-hover--vscode");
-//   vscodeMiddleBorder.classList.remove("hex-bg-color--vscode");
-//   vscodeRightBorder.classList.remove("hex-border__right-hover--vscode");
-//   vscodeIconP.classList.remove("hover-color--vscode");
-//   vscodePathOne.setAttribute("style", "fill: #fafafa");
-//   vscodePathTwo.setAttribute("style", "fill: #fafafa");
-//   vscodePathThree.setAttribute("style", "fill: #fafafa");
-// });
-
-// vscodeIconP.addEventListener("mouseover", () => {
-//   vscodeLeftBorder.classList.add("hex-border__left-hover--vscode");
-//   vscodeMiddleBorder.classList.add("hex-bg-color--vscode");
-//   vscodeRightBorder.classList.add("hex-border__right-hover--vscode");
-//   vscodeIconP.classList.add("hover-color--vscode");
-//   vscodePathOne.setAttribute("style", "fill: #0065A9");
-//   vscodePathTwo.setAttribute("style", "fill: #007ACC");
-//   vscodePathThree.setAttribute("style", "fill: #1F9CF0");
-// });
-
-// vscodeIconP.addEventListener("mouseout", () => {
-//   vscodeLeftBorder.classList.remove("hex-border__left-hover--vscode");
-//   vscodeMiddleBorder.classList.remove("hex-bg-color--vscode");
-//   vscodeRightBorder.classList.remove("hex-border__right-hover--vscode");
-//   vscodeIconP.classList.remove("hover-color--vscode");
-//   vscodePathOne.setAttribute("style", "fill: #fafafa");
-//   vscodePathTwo.setAttribute("style", "fill: #fafafa");
-//   vscodePathThree.setAttribute("style", "fill: #fafafa");
-// });
-
 // GIT ICON HOVER ANIMATION
 gitIconP.addEventListener("mouseover", () => {
   gitLeftBorder.classList.add("hex-border__left-hover--git");
@@ -411,32 +398,56 @@ gitIcon.addEventListener("mouseout", () => {
 });
 
 // CSS ICON HOVER ANIMATION
+cssSVG.addEventListener("mouseover", () => {
+  cssLeftBorder.classList.add("hex-border__left-hover--css");
+  cssMiddleBorder.classList.add("hex-bg-color--css");
+  cssRightBorder.classList.add("hex-border__right-hover--css");
+  cssIconP.classList.add("hover-color--css");
+  cssColor1.setAttribute("style", "fill: #1572B6");
+  cssColor2.setAttribute("style", "fill: #33A9DC");
+  cssColor3.setAttribute("style", "fill: #fff");
+  cssColor4.setAttribute("style", "fill: #EBEBEB");
+  cssColor5.setAttribute("style", "fill: #fff");
+  cssColor6.setAttribute("style", "fill: #EBEBEB");
+});
+
+cssSVG.addEventListener("mouseout", () => {
+  cssLeftBorder.classList.remove("hex-border__left-hover--css");
+  cssMiddleBorder.classList.remove("hex-bg-color--css");
+  cssRightBorder.classList.remove("hex-border__right-hover--css");
+  cssIconP.classList.remove("hover-color--css");
+  cssColor1.setAttribute("style", "fill: #fafafa");
+  cssColor2.setAttribute("style", "fill: #fafafa");
+  cssColor3.setAttribute("style", "fill: #272727");
+  cssColor4.setAttribute("style", "fill: #272727");
+  cssColor5.setAttribute("style", "fill: #272727");
+  cssColor6.setAttribute("style", "fill: #272727");
+});
+
 cssIconP.addEventListener("mouseover", () => {
   cssLeftBorder.classList.add("hex-border__left-hover--css");
   cssMiddleBorder.classList.add("hex-bg-color--css");
   cssRightBorder.classList.add("hex-border__right-hover--css");
-  cssIcon.classList.add("hover-color--css");
+  cssIconP.classList.add("hover-color--css");
+  cssColor1.setAttribute("style", "fill: #1572B6");
+  cssColor2.setAttribute("style", "fill: #33A9DC");
+  cssColor3.setAttribute("style", "fill: #fff");
+  cssColor4.setAttribute("style", "fill: #EBEBEB");
+  cssColor5.setAttribute("style", "fill: #fff");
+  cssColor6.setAttribute("style", "fill: #EBEBEB");
 });
 
 cssIconP.addEventListener("mouseout", () => {
   cssLeftBorder.classList.remove("hex-border__left-hover--css");
   cssMiddleBorder.classList.remove("hex-bg-color--css");
   cssRightBorder.classList.remove("hex-border__right-hover--css");
-  cssIcon.classList.remove("hover-color--css");
-});
-
-cssIcon.addEventListener("mouseover", () => {
-  cssLeftBorder.classList.add("hex-border__left-hover--css");
-  cssMiddleBorder.classList.add("hex-bg-color--css");
-  cssRightBorder.classList.add("hex-border__right-hover--css");
-  cssIconP.classList.add("hover-color--css");
-});
-
-cssIcon.addEventListener("mouseout", () => {
-  cssLeftBorder.classList.remove("hex-border__left-hover--css");
-  cssMiddleBorder.classList.remove("hex-bg-color--css");
-  cssRightBorder.classList.remove("hex-border__right-hover--css");
   cssIconP.classList.remove("hover-color--css");
+  cssColor1.setAttribute("style", "fill: #fafafa");
+  cssColor2.setAttribute("style", "fill: #fafafa");
+  cssColor3.setAttribute("style", "fill: #272727");
+  cssColor4.setAttribute("style", "fill: #272727");
+  cssColor5.setAttribute("style", "fill: #272727");
+  cssColor6.setAttribute("style", "fill: #272727");
 });
 
 // SASS ICON HOVER ANIMATION
@@ -531,32 +542,41 @@ tailwindIconP.addEventListener("mouseout", () => {
 });
 
 // JAVASCRIPT ICON HOVER ANIMATION
+
+jsSVG.addEventListener("mouseover", () => {
+  jsLeftBorder.classList.add("hex-border__left-hover--js");
+  jsMiddleBorder.classList.add("hex-bg-color--js");
+  jsRightBorder.classList.add("hex-border__right-hover--js");
+  jsIconP.classList.add("hover-color--js");
+  jsColor1.setAttribute("style", "fill: #F0DB4F");
+  jsColor2.setAttribute("style", "fill: #323330");
+});
+
+jsSVG.addEventListener("mouseout", () => {
+  jsLeftBorder.classList.remove("hex-border__left-hover--js");
+  jsMiddleBorder.classList.remove("hex-bg-color--js");
+  jsRightBorder.classList.remove("hex-border__right-hover--js");
+  jsIconP.classList.remove("hover-color--js");
+  jsColor1.setAttribute("style", "fill: #fff");
+  jsColor2.setAttribute("style", "fill: #272727");
+});
+
 jsIconP.addEventListener("mouseover", () => {
   jsLeftBorder.classList.add("hex-border__left-hover--js");
   jsMiddleBorder.classList.add("hex-bg-color--js");
   jsRightBorder.classList.add("hex-border__right-hover--js");
-  jsIcon.classList.add("hover-color--js");
+  jsIconP.classList.remove("hover-color--js");
+  jsColor1.setAttribute("style", "fill: #F0DB4F");
+  jsColor2.setAttribute("style", "fill: #323330");
 });
 
 jsIconP.addEventListener("mouseout", () => {
   jsLeftBorder.classList.remove("hex-border__left-hover--js");
   jsMiddleBorder.classList.remove("hex-bg-color--js");
   jsRightBorder.classList.remove("hex-border__right-hover--js");
-  jsIcon.classList.remove("hover-color--js");
-});
-
-jsIcon.addEventListener("mouseover", () => {
-  jsLeftBorder.classList.add("hex-border__left-hover--js");
-  jsMiddleBorder.classList.add("hex-bg-color--js");
-  jsRightBorder.classList.add("hex-border__right-hover--js");
-  jsIconP.classList.add("hover-color--js");
-});
-
-jsIcon.addEventListener("mouseout", () => {
-  jsLeftBorder.classList.remove("hex-border__left-hover--js");
-  jsMiddleBorder.classList.remove("hex-bg-color--js");
-  jsRightBorder.classList.remove("hex-border__right-hover--js");
   jsIconP.classList.remove("hover-color--js");
+  jsColor1.setAttribute("style", "fill: #fff");
+  jsColor2.setAttribute("style", "fill: #272727");
 });
 
 // REACT ICON HOVER ANIMATION
@@ -616,6 +636,47 @@ figmaIcon.addEventListener("mouseout", () => {
   figmaRightBorder.classList.remove("hex-border__right-hover--figma");
   figmaIconP.classList.remove("hover-color--figma");
 });
+
+// VS CODE SVG HOVER ANIMATION
+// vscodeSVG.addEventListener("mouseover", () => {
+//   vscodeLeftBorder.classList.add("hex-border__left-hover--vscode");
+//   vscodeMiddleBorder.classList.add("hex-bg-color--vscode");
+//   vscodeRightBorder.classList.add("hex-border__right-hover--vscode");
+//   vscodeIconP.classList.add("hover-color--vscode");
+//   vscodePathOne.setAttribute("style", "fill: #0065A9");
+//   vscodePathTwo.setAttribute("style", "fill: #007ACC");
+//   vscodePathThree.setAttribute("style", "fill: #1F9CF0");
+// });
+
+// vscodeSVG.addEventListener("mouseout", () => {
+//   vscodeLeftBorder.classList.remove("hex-border__left-hover--vscode");
+//   vscodeMiddleBorder.classList.remove("hex-bg-color--vscode");
+//   vscodeRightBorder.classList.remove("hex-border__right-hover--vscode");
+//   vscodeIconP.classList.remove("hover-color--vscode");
+//   vscodePathOne.setAttribute("style", "fill: #fafafa");
+//   vscodePathTwo.setAttribute("style", "fill: #fafafa");
+//   vscodePathThree.setAttribute("style", "fill: #fafafa");
+// });
+
+// vscodeIconP.addEventListener("mouseover", () => {
+//   vscodeLeftBorder.classList.add("hex-border__left-hover--vscode");
+//   vscodeMiddleBorder.classList.add("hex-bg-color--vscode");
+//   vscodeRightBorder.classList.add("hex-border__right-hover--vscode");
+//   vscodeIconP.classList.add("hover-color--vscode");
+//   vscodePathOne.setAttribute("style", "fill: #0065A9");
+//   vscodePathTwo.setAttribute("style", "fill: #007ACC");
+//   vscodePathThree.setAttribute("style", "fill: #1F9CF0");
+// });
+
+// vscodeIconP.addEventListener("mouseout", () => {
+//   vscodeLeftBorder.classList.remove("hex-border__left-hover--vscode");
+//   vscodeMiddleBorder.classList.remove("hex-bg-color--vscode");
+//   vscodeRightBorder.classList.remove("hex-border__right-hover--vscode");
+//   vscodeIconP.classList.remove("hover-color--vscode");
+//   vscodePathOne.setAttribute("style", "fill: #fafafa");
+//   vscodePathTwo.setAttribute("style", "fill: #fafafa");
+//   vscodePathThree.setAttribute("style", "fill: #fafafa");
+// });
 
 /* ------------------------------------
 SMOOTH SCROLL ON ANCHOR CLICK
